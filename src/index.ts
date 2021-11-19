fe1450c0b6d7bcaa6afec0ffce77ba7eae79ee4f
idris agboola <agboolaisholaidreez@gmail.com>
2021-11-19 13:07:40 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,51 @@
 import dotenv from "dotenv-safe";
-import { add } from "./helpers/add";
+import express from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:5000/api",
+      },
+    ],
+  },
+  apis: ["./routes/books.js"],
+};
+
 dotenv.config();
-console.log(add(2, 4));
+const app = express();
+
+async function main() {
+  const PORT = process.env.PORT || 5000;
+  try {
+    const specs = swaggerJsdoc(options);
+
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+    app.listen(PORT, () => {
+      console.log(`app is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
